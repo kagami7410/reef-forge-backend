95993899528668ae761ba94723984188a8e1341d
sujan <sujangurung10@gmail.com>
2025-02-06 14:47:19 +0000

    modified application class

--- src/main/java/com/spring_ecommerce/reefForge/ReefForgeBackendApplication.java
+++ src/main/java/com/spring_ecommerce/reefForge/ReefForgeBackendApplication.java
@@ -1,10 +1,16 @@
 package com.spring_ecommerce.reefForge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReefForgeBackendApplication {
+public class ReefForgeBackendApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReefForgeBackendApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ReefForgeBackendApplication.class, args);
 
