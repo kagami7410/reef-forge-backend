46beb3e4f373fc83f30c2a5d09a2e990c2588a90
sujan <sujangurung10@gmail.com>
2025-02-06 09:59:17 +0000

    added java mail sender bean

--- src/main/java/com/spring_ecommerce/reefForge/securityConfig/ApplicationConfig.java
+++ src/main/java/com/spring_ecommerce/reefForge/securityConfig/ApplicationConfig.java
@@ -2,11 +2,14 @@ package com.spring_ecommerce.reefForge.securityConfig;
 
 import com.spring_ecommerce.reefForge.models.BasketItem;
 import com.spring_ecommerce.reefForge.repository.UserRepository;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +21,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.io.InputStream;
 
 @Configuration
 @RequiredArgsConstructor
@@ -33,6 +38,31 @@ public class ApplicationConfig {
 //        return new JavaMailSenderImpl();
 //    }
 
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSender() {
+            @Override
+            public MimeMessage createMimeMessage() {
+                return null;
+            }
+
+            @Override
+            public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
+                return null;
+            }
+
+            @Override
+            public void send(MimeMessage... mimeMessages) throws MailException {
+
+            }
+
+            @Override
+            public void send(SimpleMailMessage... simpleMessages) throws MailException {
+
+            }
+        };
+    }
     @Bean
     public UserDetailsService userDetailsService() {
         return new UserDetailsService() {
